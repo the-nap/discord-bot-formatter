1113714ac0d9c68a7d306d8188b9979befa11e17
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-07-14 00:13:46 +0200

    corrected region with same logic of battle

--- linkType/region.js
+++ linkType/region.js
@@ -1,20 +1,31 @@
 import { createAPIClient } from "@wareraprojects/api";
-import { EmbedBuilder } from "discord.js";
+import { EmbedBuilder, AttachmentBuilder } from "discord.js";
 import { renderBattleMap } from "../util/renderBattleMap.js";
-import { upload } from "../util/imgbbUploader.js";
 
 export default async function getRegionData(link, id){
   const client = createAPIClient();
   const region = await client.region.getById({ regionId: id });
 
+  let startTime = performance.now();
+
   const svg = await renderBattleMap(region.position);
-  const url = await upload(svg);
+
+  let endTime = performance.now();
+  console.log(`Call to render map took ${endTime - startTime} milliseconds`);
+
+  startTime = performance.now();
+
+  const pngBuffer = await sharp(Buffer.from(svg, "utf8")).png().toBuffer();
+  const file = new AttachmentBuilder(pngBuffer, { name: "region.png" });
+
+  endTime = performance.now();
+  console.log(`Call to create attachment took ${endTime - startTime} milliseconds`);
 
   const embed = new EmbedBuilder()
   .setTitle(region.name)
   .setURL(link)
-  .setImage(url)
+  .setImage("attachments://region.png");
 
-  return [`[${region.name}](${link})`, embed]
+  return ['', embed, file]
 
 }
