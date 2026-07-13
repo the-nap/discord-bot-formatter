fadcb35e447c30800f2e9fd2640d4a97f023b067
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-07-13 12:27:23 +0200

    added some more info

--- linkType/battle.js
+++ linkType/battle.js
@@ -7,7 +7,10 @@ export default async function getBattleData(link, id){
   const client = createAPIClient();
 
   const battle = await client.battle.getById({ battleId: id });
+  const battleDetails = await client.battle.getLiveBattleData({ battleId: id });
   const region = await client.region.getById({ regionId: battle.defender.region });
+  const attacker = await client.country.getCountryById({ countryId: battle.attacker.country });
+  const defender = await client.country.getCountryById({ countryId: battle.defender.country });
 
   const svg = await renderBattleMap(region.position);
   const url = await upload(svg);
@@ -15,8 +18,12 @@ export default async function getBattleData(link, id){
   const embed = new EmbedBuilder()
   .setTitle(region.name)
   .setURL(link)
+  .addFields(
+    { name: '', value: ` ${defender.name}🛡️ ${battle.defender.wonRoundsCount} - ${battle.attacker.wonRoundsCount} ⚔️${attacker.name}` },
+    { name: '', value: `Round ${battleDetails.battle.roundIds.length} in corso` }
+  )
   .setImage(url)
 
-  return [`Battle for ${region.name}`, embed]
+  return [`Battle for [${region.name}](${link})`, embed]
 
 }
