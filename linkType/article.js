a82a9f026b3d751c34b90eac3850d4982b0d6b10
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-07-13 02:46:13 +0200

    added first embed to article

--- linkType/article.js
+++ linkType/article.js
@@ -1,5 +1,17 @@
-export default async function getArticleData(id){
+import { createAPIClient } from "@wareraprojects/api";
+import { EmbedBuilder } from "discord.js";
 
-  const article = await client.article.getArticleLiteById({ articleId: id });
+export default async function getArticleData(link, id){
+  const client = createAPIClient();
 
+  const article = await client.article.getArticleById({ articleId: id });
+  const author = await client.user.getUserById({ userId: article.author });
+
+  const embed = new EmbedBuilder()
+  .setTitle(article.title)
+  .setURL(link)
+  .setThumbnail(author.avatarUrl)
+  .setAuthor({ name:`${author.username}`});
+
+  return [`[${article.title}](${link}).Autore: ${author.username}`, embed];
 }
