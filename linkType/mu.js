d45af06ce328ec5e41a85717e087a7c5a58345a6
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-07-17 05:51:48 +0200

    added mu data

---
+++ linkType/mu.js
@@ -0,0 +1,52 @@
+import { createAPIClient } from "@wareraprojects/api";
+import { EmbedBuilder } from "discord.js";
+import formatNumber from "../util/formatNumber.js";
+import { isInWar } from "../util/skillset.js";
+
+export default async function getMuData(link, id){
+  const client = createAPIClient();
+
+  const mu = await client.mu.getById({ muId: id })
+
+  const muMembers = await Promise.all(
+    mu.members.map((member) => {
+    return client.user.getUserLite({ userId: member })
+    })
+  );
+
+  const inWar = 
+    muMembers.filter( user => isInWar(user.skills)).length;
+
+  const membersDamage = 
+    muMembers.sort((a,b) => {
+      return b.rankings.weeklyUserDamages.value - a.rankings.weeklyUserDamages.value;
+    })
+    .map((user) => {
+      return `${user.username} - ${formatNumber(user.rankings.weeklyUserDamages.value)}`
+    })
+    .join('\n');
+
+  const fields = [
+    {
+      name: '',
+      value: `
+        Danni Settimanali: ${ formatNumber(mu.rankings.muWeeklyDamages.value) }
+        Players in war: ${ inWar } / ${ muMembers.length }
+        Danno medio per war player: ${ formatNumber(mu.rankings.muWeeklyDamages.value / inWar) }
+      `
+    },
+    { 
+      name: 'Classifica',
+      value: `${membersDamage}`
+    }
+  ]
+
+  const embed = new EmbedBuilder()
+  .setTitle(mu.name)
+  .setURL(link)
+  .setThumbnail(mu.avatarUrl)
+  .addFields(fields)
+
+  return ['', embed];
+
+}
