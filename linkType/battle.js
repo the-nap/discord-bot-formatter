56495e8a61cc10398a0e0c6b0460a61d452f781d
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-07-19 00:06:18 +0200

    opt: extracted request resolver as function

--- linkType/battle.js
+++ linkType/battle.js
@@ -30,14 +30,7 @@ export default async function getBattleData(link, id){
       };
   }
 
-  const data = Object.fromEntries(
-    await Promise.all(
-      Object.entries(requests).map(async ([key, promise]) => [
-        key,
-        await promise
-      ])
-    )
-  )
+  const data = await resolveRequests(requests);
 
   let svg;
 
@@ -87,4 +80,12 @@ export default async function getBattleData(link, id){
   if(file)
     return ['', embed, file];
   return['', embed];
+}
+
+async function resolveRequests(requests) {
+    return Object.fromEntries(
+        await Promise.all(
+            Object.entries(requests).map(async ([k, p]) => [k, await p])
+        )
+    );
 }
